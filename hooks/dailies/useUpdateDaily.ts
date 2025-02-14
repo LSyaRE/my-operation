49b0912d80076d8b9f32bb8e2811192c5be91bfe
LSyaRE <jonasvillacis@yahoo.com>
2025-02-14 07:41:45 -0500

    update form

--- hooks/dailies/useUpdateDaily.ts
+++ hooks/dailies/useUpdateDaily.ts
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "expo-router";
 import { Alert } from "react-native";
 import { useForm } from "react-hook-form";
@@ -8,21 +8,36 @@ import { useDailytail } from "./useDailytail";
 
 const [dayliId, setDayliId] = useState("");
 export const useUpdateDaily = (id: string) => {
- const { dailies, error, loading } = useDailytail(+id);
+  const { dailies, error, loading } = useDailytail(+id);
+  const [dailies2, setDailies] = useState<Daily | null>(null);
+
   const {
     control,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<Daily>({
     defaultValues: {
-        id: dailies?.id,
-        escrito: dailies?.escrito,
-        observacion: dailies?.observacion,
-        tiempoEnMinutos: dailies?.tiempoEnMinutos,
-        fecha: dailies?.fecha,
-        trabajadores: dailies?.trabajadores,
+      id: dailies2?.id,
+      escrito: dailies2?.escrito,
+      observacion: dailies2?.observacion,
+      tiempoEnMinutos: dailies2?.tiempoEnMinutos,
+      fecha: dailies2?.fecha,
+      trabajadores: dailies2?.trabajadores,
+    },
+  });
+
+  useEffect(() => {
+    if (dailies) {
+      // Si los datos están disponibles, actualizamos los valores del formulario
+      setValue("id", dailies.id);
+      setValue("escrito", dailies.escrito);
+      setValue("observacion", dailies.observacion);
+      setValue("tiempoEnMinutos", dailies.tiempoEnMinutos);
+      setValue("fecha", dailies.fecha);
+      setValue("trabajadores", dailies.trabajadores);
     }
-});
+  }, [dailies, setValue]);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
 
